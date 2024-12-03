97d3c53043f330a4b06c638411b4f572c355f3e2
Amol Chavhan <amolkchavhan@gmail.com>
2024-12-03 22:15:26 +0530

    task-tracker

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/navbar";
 import TaskManager from "./components/taskManager/TaskManager";
 import About from "./components/about/About";
-import "./App.css";
 import ContactUs from "./components/contact/contactus";
+import "./App.css";
 
 const App = () => {
   return (
